83f6425a0114aee4f19de86518c229ed2e3ca0c8
rainymoon31 <rainymoon31@gmail.com>
2024-11-22 10:24:08 +0900

    로그인정보 세션 스토리지 저장

--- src/components/forum/Login.js
+++ src/components/forum/Login.js
@@ -1,7 +1,20 @@
-import { useRef } from "react";
-import { login } from "../http/http";
+import { useRef, useState } from "react";
+import { getLoginUserInfo, login } from "../http/http";
 
 export default function Login() {
+  // 새로고침해도 로그인 정보가 날아가지 않게 하기 위해 sesstionStorage에서 토큰 값을 담아놓는다.
+  const token = sessionStorage.getItem("token") || "";
+
+  // 문자열형태임. 이걸 객체리터럴로 바꿔야 함.
+  const info = JSON.parse(sessionStorage.getItem("info")) || {};
+
+  // 로그인 검증
+  const [loginState, setLoginState] = useState({
+    // 두 개의 키로 검증. 로그인했으면 토큰정보만 갱신. 내 정보를 가져왔으면 info만 갱신
+    token, // token 중복되므로 생략 , token: token,
+    info,
+  });
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
@@ -29,6 +42,22 @@ export default function Login() {
       // Browser의 데이터베이스에 token값을 저장한다.
       // sessionStorage는 브라우저가 종료되면, 데이터가 삭제된다.
       sessionStorage.setItem("token", token);
+
+      // 로그인을 했으면 setLoginState을 호출. token만 갱신하려면? 함수
+      // 기존의 객체리터러를 그대로 가져와서 복사하고 토큰값만 갱신
+      // jwt가 state에 실린다.
+      setLoginState((prevLoginState) => ({ ...prevLoginState, token }));
+
+      // login 사용자의 정보를 얻어온다. - promise를 반환하므로 await
+      const myInfoJson = await getLoginUserInfo();
+      // 세션스토리지에 바디 정보를 넣어준다.
+      sessionStorage.setItem("info", JSON.stringify(myInfoJson.body));
+
+      //마찬가지로 넣어준다. -> 토큰값은 유지되고 info만 갱신됨.
+      setLoginState((prevLoginState) => ({
+        ...prevLoginState,
+        info: myInfoJson.body,
+      }));
     } else {
       const errorMessage = tokenJson.errors.join("\n");
       alert(errorMessage);
--- src/components/http/http.js
+++ src/components/http/http.js
@@ -1,4 +1,10 @@
 // Login.js에서 쓸 수 있다.
+
+/**
+ * async 함수는 반드시 promise를 반환한다.
+ *
+ *
+ */
 export const login = async (email, password) => {
   // fetch 시작 - onClickLoginHandler 이벤트가 실행될때만 fetch가 시작됨. useEffect를 쓸 수 없음. 왜? 컴포넌트 훅들은 이런 함수 내에서는 쓸 수 없다.
   const loginUrl = "http://localhost:8080/token";
@@ -17,4 +23,21 @@ export const login = async (email, password) => {
   console.log(tokenJson);
 
   return tokenJson;
+};
+
+export const getLoginUserInfo = async () => {
+  // 세션스토리지에 있는 토큰의 정보를 가져온다.
+  const jwt = sessionStorage.getItem("token");
+
+  const myInfoUrl = "http://localhost:8080/api/v1/myinfo";
+
+  const response = await fetch(myInfoUrl, {
+    method: "GET",
+    headers: {
+      Authorization: jwt,
+    },
+  });
+
+  const myInfoJson = await response.json();
+  return myInfoJson;
 };
